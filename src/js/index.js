dd22690a311da1fc6b76321a1ba56bbd1acfba70
Eugene Lukianov <elukianov.web@gmail.com>
2018-07-19 13:50:55 +0700

    update structure

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@ import jQuery from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 
-$('.carouselTest').on('slide.bs.carousel', function () {
-  console.log('start slide!');
-})
+$( document ).ready(function() {
+  var slider = tns({
+    container: '.my-slider',
+    items: 3,
+    slideBy: 'page',
+  });
+});
